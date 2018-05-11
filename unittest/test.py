f92df727523b2de383ca57af5dfb2fd0d0ba5461
qiujin <312136446@qq.com>
2018-05-11 11:36:17 +0800

    Update test.py

--- unittest/test.py
+++ unittest/test.py
@@ -1,20 +1,11 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
-
-if __name__ == '__main__':
-    unittest.main()
+def inc(x):
+    return x + 1
+ 
+def test_answer():
+    assert inc(3) == 5
+ 
+def test_wcf():
+    assert inc(3) > 5
+ 
+def test_hy():
+    assert inc(3) < 5

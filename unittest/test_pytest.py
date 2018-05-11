85b4b455c11a901ac76d9f4b90119c36e53aa5e9
qiujin <312136446@qq.com>
2018-05-11 11:46:04 +0800

    Update test_pytest.py

--- unittest/test_pytest.py
+++ unittest/test_pytest.py
@@ -1,8 +1,11 @@
-class TestClass:  
-    def test_one(self):  
-        x = "this"  
-        assert 'h' in x  
-  
-    def test_two(self):  
-        x = "hello"  
-        assert hasattr(x, 'check') 
+def inc(x):
+    return x + 1
+ 
+def test_answer():
+    assert inc(3) == 5
+ 
+def test_wcf():
+    assert inc(3) > 5
+ 
+def test_hy():
+    assert inc(3) < 5
